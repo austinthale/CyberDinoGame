e835985f3721be5239650b3e8f594bbb8411a1cd
Unknown <jameson.ballard21@gmail.com>
2017-04-25 13:47:39 -0600

    Unity 5.6 used

---
+++ CyberDino/CyberDino/Assets/Scenes/Loading Page_JB/Assets/SpawnHall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHall : MonoBehaviour {
+
+    public GameObject Hall;
+    public float spawnTime = 7f;
+    public GameObject emptyObject;
+ 
+    void Start()
+    {
+        InvokeRepeating("SpawnBall", spawnTime, spawnTime);
+    }
+
+    void SpawnBall()
+    {
+        var newBall = GameObject.Instantiate(Hall, emptyObject.transform.position, Quaternion.Euler(0, 180, 0));
+    }
+}
