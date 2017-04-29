a4e00bfb13c3e2256e06d82ac58bff3cb3e4aaca
Unknown <jacobrressler@gmail.com>
2017-04-29 14:05:30 -0600

    Jakes awsome stuff

---
+++ CyberDino/Assets/Scripts/Movement_JR.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_JR : MonoBehaviour {
+
+    public float speed;
+    public float jumps;
+    public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxisRaw("Horizontal") * turnSpeed * Time.deltaTime,0);
+        jump();
+
+	}
+    void jump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().AddForce(0, jumps, 0);
+        }
+    }
+}
