0866faa91da3e085ec32cab7e3ee614fa58afcac
Justin Bezzant <justinbezzant@gmail.com>
2017-05-03 17:59:21 -0600

    setup selection screen with chris's controller

--- CyberDino/Assets/Scripts/Selection Screen Scripts_JusB/DinoHandler.cs
+++ CyberDino/Assets/Scripts/Selection Screen Scripts_JusB/DinoHandler.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class DinoHandler : MonoBehaviour {
-
+	public GameObject highlightBox;
+	private MoveHiglightBox whichDino;
+	public GameObject[] prefabList;
+	public GameObject currentDino;
 	// Use this for initialization
 	void Start () {
-	
+		whichDino = highlightBox.GetComponent<MoveHiglightBox> ();
+		whichDino.changeDino += SetPosititon;
+		currentDino = prefabList[0];
 	}
-	
+
+	public void SetPosititon(int myPostition)
+	{
+		Debug.Log ("made it");
+		currentDino = prefabList[myPostition];
+
+	}
 	// Update is called once per frame
-	void Update () {
-	
-	}
 }
