1511616bfad76493402f975f5cab628bd9bfb582
Unknown <bbqdpickles@gmail.com>
2017-05-03 18:26:40 -0600

    Fixed Selection Manager class name

--- CyberDino/Assets/Scripts/SelectManager_AH.cs
+++ CyberDino/Assets/Scripts/SelectManager_AH.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic; //allows use of generic lists
 
-public class SelectManager : MonoBehaviour
+public class SelectManager_AH : MonoBehaviour
 {
     public List<GameObject> listDinoManagers;
     private int count;
